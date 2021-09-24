875a44623e397d0cbe587e9ee5d4d66932a101a9
Benjamin wilson <Benjiwilson7991@gmail.com>
2021-09-24 02:33:04 +0100

    Slack demo with kivymd

---
+++ mainmd.py
@@ -0,0 +1,18 @@
+from kivymd.app import MDApp
+from kivymd.uix.floatlayout import MDFloatLayout
+#from kivy.utils import get_color_from_hex
+
+
+
+class Home(MDFloatLayout):
+    pass
+
+
+class DemoApp(MDApp):
+
+    def build(self):
+        return Home()
+
+
+if __name__ == '__main__':
+    DemoApp().run()
